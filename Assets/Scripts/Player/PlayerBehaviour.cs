782f3002761ec428fbbd6e08effb681c0e547238
Semo <ReneLoescher@gmx.net>
2021-01-29 22:49:18 +0100

    picking up legs changes visuals of character and gives ability to jump

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,7 +11,21 @@ namespace GGJ2021.Player
         [SerializeField] private float movementSpeed;
         [SerializeField] private float jumpForce;
 
+        [SerializeField] private bool canJump;
+
+        [SerializeField] private GameObject headRendererPrefab;
+        [SerializeField] private GameObject legsRendererPrefab;
+
         private Vector3 currentMovement = Vector3.zero;
+        private ContactFilter2D _contactFilter2D;
+        private BoxCollider2D _boxCollider2D;
+
+        private void Start()
+        {
+            _contactFilter2D = new ContactFilter2D();
+            _contactFilter2D.SetLayerMask(LayerMask.GetMask($"Ground"));
+            _boxCollider2D = GetComponentInChildren<BoxCollider2D>();
+        }
 
         public void OnMovement(InputAction.CallbackContext callbackContext)
         {
@@ -21,7 +36,7 @@ namespace GGJ2021.Player
 
         public void OnJump(InputAction.CallbackContext callbackContext)
         {
-            if (callbackContext.performed)
+            if (callbackContext.performed && canJump && IsOnGround())
             {
                 _rigidbody2D.AddForce(transform.up * jumpForce);
                 //_rigidbody2D.velocity = new Vector2(0f, jumpForce);
@@ -36,6 +51,26 @@ namespace GGJ2021.Player
         private void ProcessMovement()
         {
             transform.Translate(currentMovement.x, currentMovement.y, 0f);
+        }
+
+        private bool IsOnGround()
+        {
+            List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
+            return _boxCollider2D.Cast(Vector2.down, _contactFilter2D, hits, 0.1f) > 0;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Legs"))
+            {
+                canJump = true;
+                Destroy(other.gameObject);
+                
+                Destroy(_boxCollider2D.gameObject);
+                GameObject newRenderer = Instantiate(legsRendererPrefab, transform);
+                _boxCollider2D = newRenderer.GetComponent<BoxCollider2D>();
+            }
         }
     }
 }
