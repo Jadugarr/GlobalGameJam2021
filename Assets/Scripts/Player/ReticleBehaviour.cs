002bb8ecf62981753f83731449b04bc71aba6e93
Semo <ReneLoescher@gmx.net>
2021-01-30 15:09:07 +0100

    hiding reticle when on the player and using a gamepad; fixed flying bug when using gamepad

--- Assets/Scripts/Player/ReticleBehaviour.cs
+++ Assets/Scripts/Player/ReticleBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace GGJ2021.Player
@@ -7,9 +8,16 @@ namespace GGJ2021.Player
     {
         [SerializeField] private GameObject playerObject;
         [SerializeField] private float maxDistance;
+        [SerializeField] private SpriteRenderer reticleRenderer;
+
+        private void Start()
+        {
+            reticleRenderer.enabled = false;
+        }
 
         public void OnLook(InputAction.CallbackContext callbackContext)
         {
+            reticleRenderer.enabled = true;
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             if (Vector2.Distance(playerObject.transform.position, mousePosition) <= maxDistance)
             {
@@ -25,6 +33,8 @@ namespace GGJ2021.Player
         public void OnGamePadMove(InputAction.CallbackContext callbackContext)
         {
             Vector2 dir = callbackContext.ReadValue<Vector2>();
+
+            reticleRenderer.enabled = dir != Vector2.zero;
             transform.position = (Vector2)playerObject.transform.position + dir * maxDistance;
         }
     }
