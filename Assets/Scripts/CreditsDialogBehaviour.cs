83fb99c522cbf30d54f3fa7ce531ca551ba52bde
Semo <ReneLoescher@gmx.net>
2021-01-31 12:58:46 +0100

    main menu stuff, replaced sprites

---
+++ Assets/Scripts/CreditsDialogBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class CreditsDialogBehaviour : MonoBehaviour
+    {
+        [SerializeField] private GameObject mainMenu;
+
+        [SerializeField] private Button backButton;
+
+        private void Start()
+        {
+            backButton.onClick.AddListener(OnBackButtonClicked);
+        }
+
+        private void OnBackButtonClicked()
+        {
+            mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        public void OnCancelPressed(InputAction.CallbackContext callbackContext)
+        {
+            mainMenu.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/MainMenuBehaviour.cs
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -9,11 +9,21 @@ namespace DefaultNamespace
     {
         [SerializeField] private Button startGameButton;
         [SerializeField] private Button quitGameButton;
+        [SerializeField] private Button creditsButton;
+
+        [SerializeField] private GameObject creditsDialog;
 
         private void Start()
         {
             startGameButton.onClick.AddListener(OnStartGameClicked);
             quitGameButton.onClick.AddListener(OnQuitGameClicked);
+            creditsButton.onClick.AddListener(OnCreditsClicked);
+        }
+
+        private void OnCreditsClicked()
+        {
+            creditsDialog.SetActive(true);
+            gameObject.SetActive(false);
         }
 
         private void OnQuitGameClicked()
